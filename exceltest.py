a4d2972579397ebcaecab63c95283bb3cfa569d4
Godwin Liu <godwin.liu@gmail.com>
2018-01-17 08:13:40 -0500

    Initial commit

---
+++ exceltest.py
@@ -0,0 +1,28 @@
+# exceltest.py
+#
+# Excel import test
+# G Liu, 2018-Jan-16
+
+import pandas as pd
+file = 'testdata.xlsx'
+
+# load the spreadsheet
+x1 = pd.ExcelFile(file)
+
+print(x1.sheet_names)
+
+# load into a DataFrame
+df = x1.parse('data')
+
+#print(df)
+print("\n\nColumns:\n")
+cols = df.columns
+print(cols)
+
+print("\n\nIndex:\n")
+index = df.index
+print(index)
+
+print("\n\nValues:\n")
+val = df.values
+print(val)
